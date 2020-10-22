e775b0850ca59b115737f324d7b8f6f96a715a1a
AdrianNiewiadomski <kontakt@adrianniewiadomski.pl>
2020-10-22 19:19:40 +0200

    New intro - work in progress.

---
+++ js/model.js
@@ -0,0 +1,24 @@
+// jshint esversion: 6
+
+class Model{
+
+    constructor(){
+        
+        this.wierzcholki = [];
+        this.sciany = [];
+
+        
+
+        this.min = min;
+        this.max = max;
+    }
+
+    getWierzcholki(){
+        return this.wierzcholki;
+    }
+
+    getSciany(){
+        return this.sciany;
+    }
+
+}
--- js/objLoader.js
+++ js/objLoader.js
@@ -16,11 +16,65 @@ function loadModel(){
             var tekst = this.responseText;
             console.log(tekst);
 
-            // modele = wczytajObiekt(tekst, nazwa);
+            modele = wczytajObiekt(tekst, nazwa);
             // dodajdoSceny(modele);
         }
     };
 
     xhttp.open("GET", name, true);
     xhttp.send();
+}
+
+function wczytajObiekt(linia, nazwa){
+    var linijki = linia.split("\n");
+
+    var model;
+    //-------------------------------------- ponizsze oproznia tabele i wpisuje nowe wartosci ---------------------------
+    var wierzcholki = [];
+    var sciany = [];
+    var min=[];
+    var max=[];
+    min[0]=Number.POSITIVE_INFINITY;
+    min[1]=Number.POSITIVE_INFINITY;
+    min[2]=Number.POSITIVE_INFINITY;
+    max[0]=Number.NEGATIVE_INFINITY;
+    max[1]=Number.NEGATIVE_INFINITY;
+    max[2]=Number.NEGATIVE_INFINITY;
+
+    for(var i=0;i<linijki.length;i++){
+
+        if((linijki[i].charAt(0) == 'v') && (linijki[i].charAt(1) == ' ')){
+
+            var wierzcholek=[];
+            var text = linijki[i].replace(/  /g,' ');
+
+            wierzcholek[0]=parseFloat(text.split(" ")[1]);
+            wierzcholek[1]=parseFloat(text.split(" ")[2]);
+            wierzcholek[2]=parseFloat(text.split(" ")[3]);
+
+            if(wierzcholek[0]>max[0]) max[0]=wierzcholek[0];
+            if(wierzcholek[1]>max[1]) max[1]=wierzcholek[1];
+            if(wierzcholek[2]>max[2]) max[2]=wierzcholek[2];
+            if(wierzcholek[0]<min[0]) min[0]=wierzcholek[0];
+            if(wierzcholek[1]<min[1]) min[1]=wierzcholek[1];
+            if(wierzcholek[2]<min[2]) min[2]=wierzcholek[2];
+
+            wierzcholki.push(wierzcholek);
+        }
+        if((linijki[i].charAt(0) == 'f') && (linijki[i].charAt(1) == ' ')){
+
+            var text = linijki[i].replace(/  /g,' ');
+            var sciana = [];
+
+            sciana[0]=parseInt(text.split(" ")[1]);
+            sciana[1]=parseInt(text.split(" ")[2]);
+            sciana[2]=parseInt(text.split(" ")[3]);
+
+            sciany.push(sciana);
+        }
+    }
+
+    var model = [];
+    model.push(new Model(nazwa, 999, "#ffffff", wierzcholki, sciany, min, max));
+    return model;
 }
